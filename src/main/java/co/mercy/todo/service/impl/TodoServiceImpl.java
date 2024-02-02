5bc2996c3637d63183328305455d4926a1c8edf5
mmutuku <mmutuku@safaricom.co.ke>
2024-02-02 15:38:56 +0300

    add missing @Service annotation in todoserviceimpl;add db configs

--- src/main/java/co/mercy/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/co/mercy/todo/service/impl/TodoServiceImpl.java
@@ -2,9 +2,11 @@ package co.mercy.todo.service.impl;
 
 import co.mercy.todo.model.Todo;
 import co.mercy.todo.service.TodoService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TodoServiceImpl implements TodoService {
     @Override
     public List<Todo> getAll() {
