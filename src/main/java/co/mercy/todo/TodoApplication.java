47eeecef0a49060976a0dad690364ce753e34c15
mmutuku <mmutuku@safaricom.co.ke>
2024-02-02 15:12:13 +0300

    print spring version

--- src/main/java/co/mercy/todo/TodoApplication.java
+++ src/main/java/co/mercy/todo/TodoApplication.java
@@ -2,12 +2,14 @@ package co.mercy.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+		System.out.println("Spring Version:" + SpringVersion.getVersion());
 	}
 
 }
